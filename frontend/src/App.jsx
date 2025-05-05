6b5b9deee395743a3c61e0a0e90e7175ce605d07
Bittu Kumar <bk7355583@gmail.com>
2025-05-05 18:40:19 +0530

    file setup completed.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './index.css'
+
+const App = () => {
+  return (
+    <>
+    <div className='text-red-500  flex justify-around '>
+     <div>1</div>
+     <div>2</div>
+     <div>3</div>
+     <div>4</div>
+     <div>1</div>
+     <div>2</div>
+     <div>3</div>
+     <div>4</div>
+    </div>
+    <div className='text-red-500  flex justify-around '>
+     <div>1</div>
+     <div>2</div>
+     <div>3</div>
+     <div>4</div>
+     <div>1</div>
+     <div>2</div>
+     <div>3</div>
+     <div>4</div>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
